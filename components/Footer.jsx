0fcf62f3abdf1afb2d96da7cca9361fc37b03fb5
armmieyy <armmiest@gmail.com>
2022-08-21 20:38:20 +0700

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-blue-800">
+      <span class="text-sm text-white sm:text-center dark:text-white">
+        © 2022{" "}
+        <a href="" class="hover:underline">
+          ArmMie™
+        </a>
+        . All Rights Reserved.
+      </span>
+    </footer>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+
+export default function Example() {
+  return (
+    <nav className="flex items-center flex-wrap bg-blue-600 p-3 ">
+        <a className="inline-flex items-center p-2 mr-4 ">
+          <div className="text-2xl text-white font-bold uppercase tracking-wide">
+            Example
+            <span className="text-xl text-yellow-500 ml-1">
+            </span>
+          </div>
+        </a>
+      </nav>
+  )
+}
