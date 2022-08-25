9db6e8960d6b133ded300d4d8040ba633891c255
armmieyy <armmiest@gmail.com>
2022-08-25 23:41:51 +0700

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-blue-800">
+    <footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-blue-700 border-solid border-2 border-lime-400">
       <span class="text-sm text-white sm:text-center dark:text-white">
         © 2022{" "}
         <a href="" class="hover:underline">
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,76 @@
+import Link from "next/link";
 
 export default function Example() {
   return (
-    <nav className="flex items-center flex-wrap bg-blue-600 p-3 ">
+    <>
+      <nav className="flex items-center flex-wrap bg-blue-700 p-2 ">
         <a className="inline-flex items-center p-2 mr-4 ">
           <div className="text-2xl text-white font-bold uppercase tracking-wide">
             Example
-            <span className="text-xl text-yellow-500 ml-1">
-            </span>
           </div>
         </a>
+        <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+            <Link href="/">
+              <a
+                className={
+                  "lg:inline-flex lg:w-auto w-full px-4 py-2 rounded text-lg text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white "
+                }
+              >
+                หน้าหลัก
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a
+                className={
+                  "lg:inline-flex lg:w-auto w-full px-4 py-2 rounded text-lg text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white "
+                }
+              >
+                Blog
+              </a>
+            </Link>
+            <Link href="/">
+              <a
+                className={
+                  "lg:inline-flex lg:w-auto w-full px-4 py-2 rounded text-lg text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white"
+                }
+              >
+                ช่วยเหลือ
+              </a>
+            </Link>
+            <Link href="/">
+              <a
+                className={
+                  "lg:inline-flex lg:w-auto w-full px-4 py-2 mr-4 rounded text-lg text-white font-bold items-center justify-center hover:bg-blue-500 hover:text-white"
+                }
+              >
+                ติดต่อเรา
+              </a>
+            </Link>
+            |
+            <span className="ml-4">
+              <Link href={"/"} passHref>
+                <a
+                  className={
+                    "lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-lg text-white font-bold items-center justify-center hover:bg-green-500 hover:text-white"
+                  }
+                >
+                  Setting
+                </a>
+              </Link>
+            </span>
+
+            <span className="">
+              <Link href={"/"} passHref>
+                <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-lg text-white font-bold items-center justify-center hover:bg-red-600 hover:text-white">
+                  LogOut
+                </a>
+              </Link>
+            </span>
+          </div>
+        </div>
       </nav>
-  )
+    </>
+  );
 }
